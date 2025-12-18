8df83625276c55b329a76b5fcfbaf28d3712aa4e
krpcn1r <zaystep09@gmail.com>
2025-12-18 12:12:05 +0300

    Update ship.py

--- transport/task_3/ship.py
+++ transport/task_3/ship.py
@@ -3,5 +3,8 @@ from transport.task_2.vehicle import Vehicle
 
 class Ship(Vehicle):
     def __init__(self, capacity, name):
+        # Вызов конструктора базового класса Vehicle для инициализации ID и грузоподъемности
         super().__init__(capacity)
+        
+        # Установка уникального судна
         self.name = name
