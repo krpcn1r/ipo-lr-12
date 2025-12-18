b57deb7c1e2f2ec277ae62ed9713ad4f8a38a95a
krpcn1r <zaystep09@gmail.com>
2025-12-18 12:12:45 +0300

    Update truck.py

--- transport/task_3/truck.py
+++ transport/task_3/truck.py
@@ -3,5 +3,8 @@ from transport.task_2.vehicle import Vehicle
 
 class Truck(Vehicle):
     def __init__(self, capacity, color):
+        # Инициализация базовых свойств транспорта через родительский класс
         super().__init__(capacity)
+        
+        # Установка цвета 
         self.color = color
