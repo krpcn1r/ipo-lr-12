42dae55ec1f125b357c0f71ae4caa5ffbf1f312f
krpcn1r <zaystep09@gmail.com>
2025-12-18 12:10:23 +0300

    Update client.py

--- transport/task_1/client.py
+++ transport/task_1/client.py
@@ -1,5 +1,10 @@
 class Client:
     def __init__(self, name, cargo_weight, is_vip):
+        # Инициализация имени клиента
         self.name = name
+        
+        # Установка веса груза в тоннах
         self.cargo_weight = cargo_weight
+        
+        # Флаг приоритетности
         self.is_vip = is_vip
